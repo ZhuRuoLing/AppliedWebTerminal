c19db57cd02f7b89011c2c3c8e5bbe503cf99b41
DancingSnow <1121149616@qq.com>
2025-04-13 00:44:19 +0800

    build new frontend

---
+++ src/frontend/frontend/assets/composables-DwEu7j9d.js
@@ -0,0 +1,1 @@
+import{u as e,a as s}from"./node-modules_@vueuse-DPn76i3b.js";const a=e(),o=()=>{s(a),document.getElementsByTagName("html")[0].classList.toggle("dark"),a.value=!a.value};export{a as i,o as t};
