90b5dda3a2e665cad5f1a1183d3e77b358fb8b62
DancingSnow <1121149616@qq.com>
2025-04-13 21:31:57 +0800

    add strip method to Component

--- src/main/kotlin/icu/takeneko/appwebterminal/util/FormattingUtil.kt
+++ src/main/kotlin/icu/takeneko/appwebterminal/util/FormattingUtil.kt
@@ -1,7 +1,12 @@
 package icu.takeneko.appwebterminal.util
 
 import com.google.common.base.CaseFormat
-import java.util.*
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.ComponentContents
+import net.minecraft.network.chat.MutableComponent
+import net.minecraft.network.chat.contents.LiteralContents
+import net.minecraft.network.chat.contents.TranslatableContents
+import java.util.Locale
 
 fun String.toEnglishName(): String {
     return this.lowercase(Locale.ROOT).split("_")
@@ -18,4 +23,24 @@ fun String.toEnglishName(): String {
 
 fun String.toLowerCaseUnder(): String {
     return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, this)
+}
+
+
+fun Component.strip(): MutableComponent {
+    val rt: MutableComponent
+    if (neededContents(this.contents)) {
+        rt = MutableComponent.create(this.contents)
+    } else {
+        rt = MutableComponent.create(ComponentContents.EMPTY)
+    }
+    rt.style = this.style.withHoverEvent(null).withClickEvent(null).withInsertion(null)
+    this.siblings.forEach { c -> rt.append(c.strip()) }
+    return rt
+}
+
+private fun neededContents(contents: ComponentContents): Boolean {
+    if (contents == ComponentContents.EMPTY) return true
+    if (contents is LiteralContents) return true
+    if (contents is TranslatableContents) return true
+    return false
 }
