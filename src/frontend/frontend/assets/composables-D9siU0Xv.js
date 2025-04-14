2fb09b211b68aa45f80a73799fedae5f40fe5d3e
DancingSnow <1121149616@qq.com>
2025-04-15 02:38:40 +0800

    update frontend

--- src/frontend/frontend/assets/composables-D9siU0Xv.js
+++
@@ -1,1 +0,0 @@
-import{u as e,a as s}from"./node-modules_@vueuse-DHBqWc5D.js";const a=e(),o=()=>{s(a),document.getElementsByTagName("html")[0].classList.toggle("dark"),a.value=!a.value};export{a as i,o as t};
