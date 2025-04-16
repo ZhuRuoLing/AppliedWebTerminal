42db3f006a8a8adee68f7809f0efc7f812967fe0
ZhuRuoLing <jkl-963@live.com>
2025-04-17 04:04:39 +0800

    update frontend file

---
+++ src/frontend/frontend/assets/composables-CRBXEZI2.js
@@ -0,0 +1,1 @@
+import{a as e,b as s}from"./node-modules_@vueuse-BNfT2vJp.js";const a=e(),o=()=>{s(a),document.documentElement.classList.toggle("dark"),a.value=!a.value};export{a as i,o as t};
--- src/frontend/frontend/assets/composables-W6L5W7PZ.js
+++
@@ -1,1 +0,0 @@
-import{u as e,a as s}from"./node-modules_@vueuse-DuOv12GU.js";const a=e(),o=()=>{s(a),document.documentElement.classList.toggle("dark"),a.value=!a.value};export{a as i,o as t};
