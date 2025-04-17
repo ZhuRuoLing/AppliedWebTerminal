103522d9cd7e62fb75e9a0fb218f4a8cb446f54d
ZhuRuoLing <3489382552@qq.com>
2025-04-17 15:01:42 +0800

    feat: animation

--- src/main/kotlin/icu/takeneko/appwebterminal/client/gui/WebTerminalScreen.kt
+++ src/main/kotlin/icu/takeneko/appwebterminal/client/gui/WebTerminalScreen.kt
@@ -2,6 +2,8 @@ package icu.takeneko.appwebterminal.client.gui
 
 import appeng.client.gui.style.StyleManager
 import appeng.client.gui.widgets.AETextField
+import com.mojang.blaze3d.systems.RenderSystem
+import com.mojang.blaze3d.vertex.PoseStack
 import icu.takeneko.appwebterminal.AppWebTerminal
 import icu.takeneko.appwebterminal.all.networkingChannel
 import icu.takeneko.appwebterminal.compat.modernui.ModernUISupport
@@ -12,20 +14,31 @@ import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.components.Button
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.network.chat.Component
+import net.minecraft.util.Mth
 import net.minecraftforge.network.PacketDistributor
-import java.util.UUID
+import java.util.*
+import kotlin.math.PI
+import kotlin.math.cos
 
+@Suppress("PrivatePropertyName")
 class WebTerminalScreen(
     private var name: String,
     private var uuid: UUID,
     private var password: String,
     private var isOnline: Boolean,
 ) : Screen(Component.translatable("appwebterminal.screen.title")) {
-
+    private val AnimationProgressMillis = 250L
     private val texture = AppWebTerminal.location("textures/gui/blank.png")
     private lateinit var nameField: AETextField
     private lateinit var finishButton: Button
     private lateinit var passwordField: AETextField
+
+    private var animationState = AnimationState.START
+    private var animationProgress = 0L
+    private var animationStartMillis = -1L
+    private var animationEndMillis = -1L
+    private var shouldDismissScreen = false
+    private var shouldContinueRendering = true
     override fun init() {
         super.init()
         val style = StyleManager.loadStyleDoc("/screens/terminals/crafting_terminal.json")
@@ -49,7 +62,7 @@ class WebTerminalScreen(
             }
 
         finishButton = Button.builder(Component.translatable("appwebterminal.button.done")) {
-            Minecraft.getInstance().setScreen(null)
+            setupDismissAnimationState()
         }.bounds(x + 130, y + 110, 60, 20).build()
         addRenderableWidget(nameField)
         addRenderableWidget(passwordField)
@@ -57,6 +70,26 @@ class WebTerminalScreen(
     }
 
     override fun isPauseScreen(): Boolean {
+        return false
+    }
+
+    override fun keyPressed(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {
+        if (keyCode == 256) {
+            if (!shouldDismissScreen) {
+                setupDismissAnimationState()
+            }
+            return true
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers)
+    }
+
+    fun setupDismissAnimationState() {
+        shouldDismissScreen = true
+        animationState = AnimationState.DISMISS
+        animationStartMillis = -1L
+    }
+
+    override fun shouldCloseOnEsc(): Boolean {
         return false
     }
 
@@ -75,7 +108,47 @@ class WebTerminalScreen(
         guiGraphics.blit(texture, x, y, 0, 0, 195, 136)
     }
 
+    fun setupAnimationState(pose: PoseStack) {
+        if (animationState == AnimationState.NORMAL) return
+        if (animationStartMillis == -1L) {
+            animationStartMillis = System.currentTimeMillis()
+            animationEndMillis = System.currentTimeMillis() + AnimationProgressMillis
+        }
+        if (animationProgress > animationEndMillis) {
+            finishButton.setAlpha(animationState.endAlpha)
+            if (shouldDismissScreen) {
+                Minecraft.getInstance().setScreen(null)
+                shouldContinueRendering = false
+                return
+            }
+            animationState = AnimationState.NORMAL
+            return
+        }
+        animationProgress = System.currentTimeMillis()
+        val progress = (animationEndMillis - animationProgress) / AnimationProgressMillis.toFloat()
+        val progressNonlinear = (cos((progress + 1) * PI) / 2.0 + 0.5).toFloat()
+
+        val scale = Mth.lerp(1 - progressNonlinear, animationState.startScale, animationState.endScale)
+        val alpha = Mth.lerp(1 - progressNonlinear, animationState.startAlpha, animationState.endAlpha)
+        val xMove = (1 - scale) * width / 2
+        val yMove = (1 - scale) * height / 2
+        pose.translate(xMove, yMove, 0f)
+        pose.scale(scale, scale, scale)
+        //println(progressNonlinear.toString() + " " + alpha)
+        finishButton.setAlpha(alpha)
+        RenderSystem.setShaderColor(1f, 1f, 1f, alpha)
+    }
+
     override fun render(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
+        RenderSystem.enableBlend()
+        shouldContinueRendering = true
+        val pose = guiGraphics.pose()
+        pose.pushPose()
+        setupAnimationState(pose)
+        if (!shouldContinueRendering) {
+            pose.popPose()
+            return
+        }
         val x: Int = (this.width - 195) / 2
         val y: Int = (this.height - 136) / 2
         val font = Minecraft.getInstance().font
@@ -125,9 +198,36 @@ class WebTerminalScreen(
             )
         }
         super.render(guiGraphics, mouseX, mouseY, partialTick)
+        pose.popPose()
     }
 
     override fun removed() {
         networkingChannel.send(PacketDistributor.SERVER.noArg(), UpdateWebTerminalNamePacket(name, uuid, password))
+    }
+
+    enum class AnimationState {
+        START {
+            override val startScale = 1.25f
+            override val endScale = 1f
+            override val startAlpha = 0f
+            override val endAlpha = 1f
+        },
+        NORMAL {
+            override val startScale = 1f
+            override val endScale = 1f
+            override val startAlpha = 1f
+            override val endAlpha = 1f
+        },
+        DISMISS {
+            override val startScale = 1f
+            override val endScale = 1.25f
+            override val startAlpha = 1f
+            override val endAlpha = 0f
+        };
+
+        abstract val startScale: Float
+        abstract val endScale: Float
+        abstract val startAlpha: Float
+        abstract val endAlpha: Float
     }
 }
