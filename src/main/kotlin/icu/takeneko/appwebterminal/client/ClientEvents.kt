8f6994a3b22e10ae15078891b6ed15a59a551a35
ZhuRuoLing <jkl-963@live.com>
2025-05-21 03:40:29 +0800

    fix compatibility issue with forge

--- src/main/kotlin/icu/takeneko/appwebterminal/client/ClientEvents.kt
+++ src/main/kotlin/icu/takeneko/appwebterminal/client/ClientEvents.kt
@@ -48,7 +48,7 @@ fun onRenderLevelPost(event: RenderLevelStageEvent) {
     if (event.stage == RenderLevelStageEvent.Stage.AFTER_LEVEL) {
         if (Minecraft.getInstance().screen is WebTerminalScreen) {
             val mainRenderTarget = Minecraft.getInstance().mainRenderTarget
-            BlurPostProcessInstance?.apply(mainRenderTarget.frameBufferId)
+            BlurPostProcessInstance?.apply(mainRenderTarget.colorTextureId)
             mainRenderTarget.bindWrite(true)
         }
         AEKeyRenderer.instance?.next()
